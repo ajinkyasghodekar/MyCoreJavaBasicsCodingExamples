8ae2c390fba52cd192ce79de28a8dcc3f86d91d2
Ajinkya Ghodekar <ajinkyasghodekar@gmail.com>
2021-07-23 21:43:06 +0530

    Updated logic for swapping 2 number by using 2 variables.

--- LogicBasedExample/SwappingUsing2Var.java
+++ LogicBasedExample/SwappingUsing2Var.java
@@ -4,10 +4,11 @@ public class SwappingUsing2Var {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        a = b;
-        b = a;
-        System.out.println(a);
-        System.out.println(b);
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("Value of a is:" +a);
+        System.out.println("Value of b is:" +b);
 
     }
 }
