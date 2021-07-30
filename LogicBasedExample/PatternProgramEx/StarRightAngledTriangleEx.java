524b25786b7d11411b63f25c5c3bd1cf78bba043
Ajinkya Ghodekar <ajinkyasghodekar@gmail.com>
2021-07-30 20:30:49 +0530

    Worked on 2 new triangle pattern in java.

---
+++ LogicBasedExample/PatternProgramEx/StarRightAngledTriangleEx.java
@@ -0,0 +1,15 @@
+package LogicBasedExample.PatternProgramEx;
+
+public class StarRightAngledTriangleEx {
+    public static void main(String[] args) {
+        int i, j, n=6;
+        
+        for(i=0;i<n;i++){
+            for(j=0; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println(); 
+        }
+    }
+}
+
